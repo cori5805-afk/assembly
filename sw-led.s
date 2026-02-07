176c53adad4c2e0053306eb3f3b50cf1f69acab2
cori5805-afk <cori5805@colorado.edu>
2026-02-06 17:50:46 -0700

    sw-led.s

--- sw-led.s
+++ sw-led.s
@@ -1,16 +1,29 @@
 .text
-    .equ    LED_ADDR,   0xFF200000
-    .equ    SW_ADDR,    0xFF200040
-    .global _start
+.equ LEDs, 0xFF200000
+.equ SWITCHES, 0xFF200040
+.global _start
 _start:
-    movia   r2, LED_ADDR
-    movia   r3, SW_ADDR
 
-loop:
-    ldwio   r4, 0(r3)
-    
-    # Your code here
 
-    stwio   r4, 0(r2)
-    br      loop
+movia r2, LEDs # Address of LEDs
+
+movia r3, SWITCHES # Address of switches
+
+LOOP:
+
+ldwio r4, (r3) # Read the state of switches
+
+# your code here
+
+andi r5, r4, 0x1f # get rightmost bits (rightmost 4 switches) store in r5 
+
+srli r6, r4, 5 # splits switches into two separate registers
+
+andi r6, r6, 0x1f # r6 now shifted to right most bits
+
+add r4, r6, r5 # add A + B 
+
+stwio r4, (r2) # Display the state on LEDs
+
+br LOOP
 
