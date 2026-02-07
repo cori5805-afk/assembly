851dc230cc12821cbadd30e5c57b4737bde05fe5
cori5805-afk <cori5805@colorado.edu>
2026-02-06 17:51:45 -0700

    sum-n.s

--- sum-n.s
+++ sum-n.s
@@ -4,9 +4,17 @@ _start:
     ldw     r4, 0(r4)
     movi    r2, 0
 
-    # Your code here
+loop:
 
-    break
+	beq r4, r0, done # verify if all integers have been added
+	
+	add r2, r2, r4 # add r4 and r2 store into r2
+	
+	subi r4, r4, 1 # decrement r4 (N)
+	
+	br loop # if beq statement is false start the loop again
+	
+done:
 
 .data
 N:  .word 9
